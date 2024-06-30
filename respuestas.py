f4ed8db3a646416422bca02aefff49a1a7d45ef1
Santiago Langer <84828241+astrocinco@users.noreply.github.com>
2024-06-29 23:02:52 -0300

    Added: details

--- respuestas.py
+++ respuestas.py
@@ -1,9 +1,9 @@
 def load_respuestas():
-    links = ["comienzo", "arturito", "departamentos", "calendario", "deportes"]
+    links = ["comienzo", "departamentos", "faq", "calendario", "aulas", "apuntes", "deportes", "arturito", "bugs"]
     respuestas = {}
 
     for link in links:
-        path = f"'respuestas/{link}.txt'"
+        path = f"respuestas/{link}.txt"
         key = link
         with open(path, 'r', encoding="utf-8") as file:
             respuestas[key] = file.read()
